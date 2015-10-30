b9eca2a18cb3efb92854d495e8ab03bfc2cc4479
timcartwright <tim@websitesbytim.com>
2015-10-30 16:08:49 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+weatherApp.controller('mainController', ['$scope', '$location', function($scope, $location) {
+
+
+}]);
+
+
+weatherApp.controller('secondController', ['$scope','$location', function($scope, $location) {
+
+
+}]);
