952336b95efa70cd4bfb7efa5ab40e59bbdc2907
timcartwright <tim@websitesbytim.com>
2015-10-30 20:22:09 +0530

    Change link to submit button

--- app.js
+++ app.js
@@ -31,13 +31,17 @@ weatherApp.service('cityService', function() {
 });
 
 // Controllers
-weatherApp.controller('mainController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('mainController', ['$scope', '$location' 'cityService', function($scope, $location, cityService) {
 
 	$scope.city = cityService.city;
 
 	$scope.$watch('city', function() {
 		cityService.city = $scope.city;
 	});
+
+	$scope.submit = function() {
+		$location.path("/forecast");
+	}
 
 
 }]);
